18132be4beaabe07f4763616e2f9eb4682ef97f6
smob123 <smob123@hotmail.com>
2018-02-22 22:04:02 -0400

    second commit

---
+++ Sultan's Kitchen/public_html/script.js
@@ -0,0 +1,16 @@
+var expanded = false;
+function expand() {
+    if (!expanded) {
+        document.getElementById("mainNav").style.display = "inline";
+        expanded = true;
+    } else {
+        document.getElementById("mainNav").style.display = "none";
+        expanded = false;
+    }
+}
+
+function displayNavMenu() {
+    if (window.innerWidth > 480) {
+        document.getElementById("mainNav").style.display = "block";
+    }
+}
