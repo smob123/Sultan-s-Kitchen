163d0c1714fa22809fcfccef0e2acb80d566991e
smob123 <smob123@hotmail.com>
2018-02-25 22:51:24 -0400

    new commit

--- Sultan's Kitchen/public_html/script.js
+++ Sultan's Kitchen/public_html/script.js
@@ -1,4 +1,7 @@
 var expanded = false;
+var name, email, phoneNum, message;
+var elements = new Array();
+
 function expand() {
     if (!expanded) {
         document.getElementById("mainNav").style.display = "inline";
@@ -16,6 +19,40 @@ function displayNavMenu() {
 }
 
 function sendData() {
+    for (var i = 0; i < VariableList().length; i++) {
+        if (elements[i] === "") {
+            alert("Please fill empty feilds");
+            event.preventDefault();
+            return;
+        }
+    }
     alert("Message sent!");
     event.preventDefault();
+}
+
+function validateEmail(email) {
+    if (!email.checkValidity() || email.value === "") {
+        email.style.border = "2px solid red";
+    } else {
+        email.style.border = "2px solid green";
+    }
+}
+
+function validatePhone(phoneNumber) {
+    if (!phoneNumber.checkValidity() || phoneNumber.value === "") {
+        phoneNumber.style.border = "2px solid red";
+    } else {
+        phoneNumber.style.border = "2px solid green";
+    }
+}
+
+function VariableList() {
+    name = document.getElementById("nameBox");
+    email = document.getElementById("emailBox");
+    phoneNum = document.getElementById("phoneBox");
+    message = document.getElementById("messageBox");
+    
+    elements = [name.value, email.value, phoneNum.value, message.value];
+    
+    return elements;
 }
