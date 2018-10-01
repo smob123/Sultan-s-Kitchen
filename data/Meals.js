0300113808d9032cd1957f30659c76a6485e5cef
Sultan <smob123@hotmail.com>
2018-10-01 16:50:58 +0300

    added images and fixed some styling issues for the order page

--- data/Meals.js
+++ data/Meals.js
@@ -1,36 +1,63 @@
 let appetizers = {
     class: 'appetizers',
     meals: [
-        'Soup',
-         2.99,
-         'Salad',
-         4.25,
-         'Spring roll',
-         6.15
+        [
+            'images/soup.jpg',
+            'Soup',
+            2.99
+        ],
+        [
+            'images/salad.jpg',
+            'Salad',
+            4.25
+        ],
+        [
+            'images/spring roll.jpg',
+            'Spring roll',
+            6.15
+        ]
     ]
 };
 
 let main = {
     class: 'main',
     meals: [
+        [
+        'images/steak_menu.jpg',
         'steak',
-        20.99,
+        20.99
+        ],
+        [
+        'images/chicken & rice.jpg',
         'Chicken and rice',
-        18.25,
+        18.25
+        ],
+        [
+        'images/spaghetti.jpg',
         'Spaghetti',
         16.99
+        ]
     ]
 };
 
 let desserts = {
     class: 'desserts',
     meals: [
+        [
+        'images/cheesecake.jpg',
         'Cheesecake',
-        12.25,
+        12.25
+        ],
+        [
+        'images/chocolate cake.jpg',
         'Chocolate cake',
-        14.99,
+        14.99
+        ],
+        [
+        'images/icecream.jpg',
         'Ice cream',
         8.15
+        ]
     ]
 };
 
