5cb44e83676f8d75a5b94f20abb6b3341b019af4
Sultan <smob123@hotmail.com>
2019-07-06 12:51:17 +1200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,16 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Sultan-s-Kitchen/precache-manifest.0a2ddf3f4d6b7d92f2b60a81fcb3664b.js"
+  "/Sultan-s-Kitchen/precache-manifest.e8e7bef60d7953ffcde36c73c3947610.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Sultan-s-Kitchen/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sultan-s-Kitchen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
